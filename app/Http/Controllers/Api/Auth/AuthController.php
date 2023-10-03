d35187335dd1fa5c2066b76206c3c295b00e87b1
Andrian Putra Ramadan <ramadanandrian89@gmail.com>
2023-10-03 13:59:17 +0700

    fix(auth): change error response for invalid credential

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+           return $this->errorResponse('Invalid credential', 401);
         }
 
         return $this->respondWithToken($token);
